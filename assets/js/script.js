854165cb3a6273e785d55e02a556a87c77467039
guilhermesmb <guilherme.smbarbosa@gmail.com>
2023-04-04 22:11:25 -0300

    ok

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+
+document.querySelector("#qtde").addEventListener("change", calcularOrcamento)
+document.querySelector("#js").addEventListener("change", calcularOrcamento)
+
+function calcularOrcamento() {
+    let qtde = document.querySelector("#qtde").value
+    let preco = qtde * 100
+    
+    let js = document.querySelector("#js").checked
+
+    if (js == true){
+        preco = preco + (preco * 10/100)
+    }
+
+    document.querySelector("#preco").innerHTML = "R$ " + preco
+}
+
+//Manipulação do DOM
+
+//ECMAScript
