7beff490b8bcd9806dde7e7efd570eb2141189c8
ArthurTortoreli <arthurtortoreli@hotmail.com>
2023-04-04 22:12:00 -0300

    Adding checkbox js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,14 @@
 document.querySelector("#qtde").addEventListener("change", calcularOrcamento);
+document.querySelector("#js").addEventListener("change", calcularOrcamento);
 
 function calcularOrcamento() {
     let qtdePaginas = document.querySelector("#qtde").value;
     let preco = qtdePaginas * 100;
-    console.log(qtdePaginas, preco);
+
+    let js = document.querySelector("#js").checked;
+
+    js ? preco = preco + (preco * 10/100) : preco;
+
+    document.querySelector("#preco").innerHTML = `R$ ${preco}`;
 }
 
